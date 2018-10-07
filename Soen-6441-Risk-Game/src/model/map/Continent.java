cb788ad2b90105bcbb283d87ae14386fe219791c
wasabio <kerichardyann@gmail.com>
2018-10-07 15:00:39 -0400

    Updatig project structure

--- Soen-6441-Risk-Game/src/model/map/Continent.java
+++ Soen-6441-Risk-Game/src/model/map/Continent.java
@@ -1,5 +1,9 @@
 package model.map;
 
-public class Continent {
+import java.util.ArrayList;
 
+public class Continent {
+	
+	private ArrayList<Country> countries = new ArrayList<Country>();
+	private ArrayList<Continent> neighbours = new ArrayList<Continent>();
 }
