629740e7d614e4e482b708f13424adad15b15490
Bill Chen <bill1119@hotmail.com>
2018-10-08 09:38:25 +0800

    some changes in continent and Map class

--- Soen-6441-Risk-Game/src/model/map/Map.java
+++ Soen-6441-Risk-Game/src/model/map/Map.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 
 import model.utilities.FileHandler;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Observable;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+
+
 public class Map {
 	
 	private ArrayList<Continent> continents = new ArrayList<Continent>();
-	
+	public ArrayList<Country> Countries = new ArrayList<Country>();
 	private String name;
+	private String mapFilePath;
+	private String imageFilePath;
+	
+	
 
 	public void load() 
 	{
@@ -27,4 +49,23 @@ public class Map {
 		this.name = name;
 	}
 
+	/**
+	 * check whether a map contains two different format files including .map
+	 * text file and .bmp image file.
+	 * 
+	 * @return Return true if two different files are matched exactly, other
+	 *         wise return false.
+	 */
+	public boolean isDisparateImageFileDirectory() {
+		if (this.imageFilePath == null) {
+			return false;
+		}
+		if (this.mapFilePath == null) {
+			return false;
+		}
+		File mapDir = new File(this.mapFilePath).getParentFile();
+		File imgDir = new File(this.imageFilePath).getParentFile();
+		return (mapDir != null) && (imgDir != null) && (mapDir.compareTo(imgDir) != 0);
+	}
+	
 }
