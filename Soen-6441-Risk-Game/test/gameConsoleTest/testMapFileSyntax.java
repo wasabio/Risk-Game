44c3fa47b22c32944d0e4990c3c98ab6730d9b66
Bill Chen <bill1119@hotmail.com>
2018-10-21 06:13:10 +0800

    update the docs for testmapsyntax.java

--- Soen-6441-Risk-Game/test/gameConsoleTest/testMapFileSyntax.java
+++ Soen-6441-Risk-Game/test/gameConsoleTest/testMapFileSyntax.java
@@ -9,15 +9,23 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-
+/**
+ * 
+ * The test class for testing the map syntax
+ *
+ */
 public class testMapFileSyntax 
 {
 	String mapFilePath;
 	LineNumberReader in;
-	
+	 
 	@Rule
 	  public final ExpectedException exception = ExpectedException.none();
 
+	/**
+	 * Testing can or cannot find a map
+	 * @throws FileNotFoundException
+	 */
 	@Test(expected = FileNotFoundException.class)
 	public void testNotFoundMap() throws FileNotFoundException 
 	{
@@ -26,7 +34,10 @@ public class testMapFileSyntax
 		in = new LineNumberReader(new FileReader(mapFilePath));
 	}
 	
-	
+	/**
+	 * Testing whether can the program find out not suitable(missing) syntax
+	 * @throws FileNotFoundException
+	 */
 	@Test(expected = FileNotFoundException.class)
 	public void testMissingMapSection() throws FileNotFoundException
 	{
