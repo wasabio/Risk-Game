17041459a58d414cb786a647af82d8593f5b8d1e
wasabio <kerichardyann@gmail.com>
2018-10-20 18:19:58 -0400

    Update Soen-6441-Risk-Game/test/mapEditorTest/testAddCountry.java

--- Soen-6441-Risk-Game/test/mapEditorTest/testAddCountry.java
+++ Soen-6441-Risk-Game/test/mapEditorTest/testAddCountry.java
@@ -16,14 +16,21 @@ public class testAddCountry {
 	MapEditor mapEditor = new MapEditor();
 	Continent con1 = new Continent("",1);
 	Continent con2 = new Continent("",2);
+	Country ctry1 = new Country();
+	Country ctry2 = new Country();
+	
 	
 	/**
-	 * 
+	 * before
 	 */
 	@Before
 	public void Before() {
 		mapEditor.map.continents.add(con1);
 		mapEditor.map.continents.add(con2);
+		ctry1.setArmyNumber(5);
+		ctry2.setArmyNumber(2);
+		con1.addCountry(ctry1);
+		con1.addCountry(ctry2);
 	}
 	@Test
 	
@@ -32,7 +39,10 @@ public class testAddCountry {
 	 */
 	public void test() {
 		
-		
+		mapEditor.addContinent(con1);
+		mapEditor.addCountry(ctry1);
+		mapEditor.addCountry(ctry2);
+		assertEquals(mapEditor.map.countries.size(), 2);
 	}
 
 }
