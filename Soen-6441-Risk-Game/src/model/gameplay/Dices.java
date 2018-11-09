daa3313f292b2d9a4c9497540eabe5b011802a60
Bill Chen <bill1119@hotmail.com>
2018-11-10 07:53:05 +0800

    dice class doc finish

--- Soen-6441-Risk-Game/src/model/gameplay/Dices.java
+++ Soen-6441-Risk-Game/src/model/gameplay/Dices.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 
 import model.utilities.Random;
 /**
- * 
+ * The class that is dealing with dice functions
  * 
  * @author Yann Kerichard, Yueshuai Jiang, Che-Shao Chen
  */
@@ -19,19 +19,35 @@ public class Dices {
 	private int attackerLoss = 0;
 	private int defenderLoss = 0;
 	
+	/**
+	 * Dice for attack and defend player
+	 * @param new_attackerArmiesNumber army number of attacker
+	 * @param new_defenderArmiesNumber army number of defender
+	 */
 	public Dices(int new_attackerArmiesNumber, int new_defenderArmiesNumber) {
 		attackerMaxDices = Math.min(new_attackerArmiesNumber-1, 3);
 		defenderMaxDices = Math.min(new_defenderArmiesNumber, 2);
 	}
 
+	/**
+	 * the max number of dice of attacker
+	 * @return return the max number of attacker dice
+	 */
 	public int getAttackerMaxDices() {
 		return attackerMaxDices;
 	}
 	
+	/**
+	 * the max number of defender
+	 * @return return the max number of defender dice
+	 */
 	public int getDefenderMaxDices() {
 		return defenderMaxDices;
 	}
 	
+	/**
+	 * function of start rolling the dice an compare the dice number
+	 */
 	public void roll() {
 		if(attDicesNumber == 0 && defDicesNumber == 0) {	/* All-out mode */
 			attDicesNumber = attackerMaxDices;
@@ -60,21 +76,38 @@ public class Dices {
 		}
 	}
 	
+	/**
+	 * show attacker loss
+	 * @return attacker loss message
+	 */
 	public int getAttackerLoss() {
 		System.out.println("attloss:" + attackerLoss);
 		return attackerLoss;
 	}
 	
+	/**
+	 * show defender loss
+	 * @return defender loss message
+	 */
 	public int getDefenderLoss() {
 		System.out.println("defloss:" + defenderLoss);
 		return defenderLoss;
 	}
 	
+	/**
+	 * set the dice number for both side
+	 * @param attDicesNumber attacker dice num
+	 * @param defDicesNumber defender dice num
+	 */
 	public void setDicesNumber(int attDicesNumber, int defDicesNumber) {
 		this.attDicesNumber = attDicesNumber;
 		this.defDicesNumber = defDicesNumber;
 	}
 	
+	/**
+	 * A dice number constraint 1~6
+	 * @return dice number between 1~6
+	 */
 	private int rollADice() {
 		return Random.getRandomInt(1, 6);
 	}
