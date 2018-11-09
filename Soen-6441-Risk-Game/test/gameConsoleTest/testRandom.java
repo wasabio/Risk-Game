338c7b2a1553beac792a61a02bb2c33f9a6b23db
Bill Chen <bill1119@hotmail.com>
2018-11-10 06:32:29 +0800

    update testcases

---
+++ Soen-6441-Risk-Game/test/gameConsoleTest/testRandom.java
@@ -0,0 +1,25 @@
+package gameConsoleTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+import model.utilities.Random;
+
+public class testRandom 
+{
+	@Test
+	public void testRandomFunc() 
+	{
+		int i = 5;
+		int m = 10;
+		Random.getRandomInt(i, m);
+		
+		
+		assertTrue(Random.getRandomInt(i, m)<=10);
+		assertTrue(5<=Random.getRandomInt(i, m));
+	}
+	
+}
