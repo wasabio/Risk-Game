dc5bfbccf98d7241d3ef5877cfe8ed5833399825
Bill Chen <bill1119@hotmail.com>
2018-11-10 07:57:20 +0800

    done the phase doc

--- Soen-6441-Risk-Game/src/model/gameplay/Phase.java
+++ Soen-6441-Risk-Game/src/model/gameplay/Phase.java
@@ -4,7 +4,7 @@ package model.gameplay;
 import java.util.Observable;
 /**
  * 
- * 
+ * The class for observer phases
  * @author Yann Kerichard, Yueshuai Jiang, Che-Shao Chen
  */
 public class Phase extends Observable{
@@ -12,6 +12,13 @@ public class Phase extends Observable{
 	Player p;
 	String action="";
 	int army=-1;
+	
+	/**
+	 * Change phase states in the game
+	 * @param newPhase the nest state phase
+	 * @param newPlayer next player
+	 * @param armyNum the army num
+	 */
 	public void setPhase(String newPhase,Player newPlayer,int armyNum) {
 		this.phase = newPhase;
 		this.p = newPlayer;
@@ -19,15 +26,35 @@ public class Phase extends Observable{
 		setChanged();
 		notifyObservers();
 	}
+	
+	/**
+	 * get the current phase
+	 * @return return the current phase
+	 */
 	public String getPhase() {
 		return phase;
 	}
+	
+	/**
+	 * get the current player
+	 * @return the current player
+	 */
 	public Player getPlayer() {
 		return p;
 	}
+	
+	/**
+	 * get the current player's action
+	 * @return the current player's action
+	 */
 	public String getAction() {
 		return action;
 	}
+	
+	/**
+	 * set the action
+	 * @param str the action name
+	 */
 	public void setAction(String str) {
 		action = str;
 	}
