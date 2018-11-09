d9cdb4955972157e7580ba7c36afc2ba08ed6050
jacklaojiang <35578667+jacklaojiang@users.noreply.github.com>
2018-11-08 21:12:03 -0500

    test cases

---
+++ Soen-6441-Risk-Game/test/classTests/testPlayer.java
@@ -0,0 +1,14 @@
+package classTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testPlayer {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
