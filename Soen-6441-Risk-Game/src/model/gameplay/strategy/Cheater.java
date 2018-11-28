f7d33069edb8623017bb6b37183252560f84a10f
wasabio <kerichardyann@gmail.com>
2018-11-28 18:15:06 -0500

    Update Cheater.java

--- Soen-6441-Risk-Game/src/model/gameplay/strategy/Cheater.java
+++ Soen-6441-Risk-Game/src/model/gameplay/strategy/Cheater.java
@@ -1,5 +1,9 @@
 package model.gameplay.strategy;
 
+import java.util.Iterator;
+import java.util.ListIterator;
+
+import model.gameplay.Player;
 import model.map.Country;
 
 public class Cheater extends ConcreteStrategy implements Strategy {
@@ -13,21 +17,30 @@ public class Cheater extends ConcreteStrategy implements Strategy {
 			{
 				int army = c.getArmyNumber() * 2;
 				c.setArmyNumber(army);
+				map.getPhase().setAction(player.getName() + " reinforced " + c.getArmyNumber() + " army in " + c.getName() + "\n");		
 			}
-		}		
+		}
 	}
 
 	@Override
 	public void attack() {
 		map.getPhase().setPhase("Attack phase", player);
 		
-		for(Country c : player.ownedCountries) {
+		ListIterator<Country> i = player.ownedCountries.listIterator();
+		
+		while (i.hasNext()) 
+		{
+			Country c = i.next();
 			for(Country neighbor : c.neighbors)
 			{
 				if (neighbor.getPlayer() != player)
 				{
+					Player defender = neighbor.getPlayer();
+					defender.ownedCountries.remove(neighbor);
+					i.add(neighbor);
 					neighbor.setPlayer(player);
-					player.ownedCountries.add(neighbor);
+					map.addArmiesToCountry(neighbor, (-neighbor.getArmyNumber() + 1));	//Setting army number to 1
+					map.getPhase().setAction(map.getPhase().getAction() + c.getName() + "(" + player.getName() + ") conquered " + neighbor.getName() + " and moved 1 army\n");
 				}
 			}
 		}
@@ -36,8 +49,8 @@ public class Cheater extends ConcreteStrategy implements Strategy {
 	@Override
 	public void fortify() {
 		map.getPhase().setPhase("Fortification phase", player);
-		
-		for(Country c : player.ownedCountries)
+		/*
+		for(Country c : player.ownedCountries) {
 			for( Country neighbor : c.neighbors)
 			{
 				if( neighbor.getPlayer() != player)
@@ -47,6 +60,7 @@ public class Cheater extends ConcreteStrategy implements Strategy {
 					break;
 				}
 			}
+		}*/
 	}
 
 }
