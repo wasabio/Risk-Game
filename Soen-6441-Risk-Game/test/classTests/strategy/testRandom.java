2f8c69d9691a85a735cc4bf4046d15c2200fa4e5
jacklaojiang <35578667+jacklaojiang@users.noreply.github.com>
2018-11-28 23:36:09 -0500

    Update testRandom.java

--- Soen-6441-Risk-Game/test/classTests/strategy/testRandom.java
+++ Soen-6441-Risk-Game/test/classTests/strategy/testRandom.java
@@ -85,10 +85,10 @@ public class testRandom {
 		assertEquals(10,p1.getArmies());
 		p1.reinforce();
 		
-		assertEquals(18,(cty1.getArmyNumber()+cty3.getArmyNumber()+p1.getArmies()));
-		//assertEquals(10,p1.getArmies());
+		assertEquals(14,(cty1.getArmyNumber()+cty3.getArmyNumber()+p1.getArmies()));
+		
 		assertEquals(20,p2.getArmies());
-		//assertEquals(2,cty2.getArmyNumber());
+
 		
 	}
 	/*
