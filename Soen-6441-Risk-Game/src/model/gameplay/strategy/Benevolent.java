df9886fbe558918f89f4ff26ede665910d6e559f
wasabio <kerichardyann@gmail.com>
2018-11-29 15:08:56 -0500

    Update Benevolent.java

--- Soen-6441-Risk-Game/src/model/gameplay/strategy/Benevolent.java
+++ Soen-6441-Risk-Game/src/model/gameplay/strategy/Benevolent.java
@@ -1,37 +1,43 @@
 package model.gameplay.strategy;
 
-import java.util.ArrayList;
 import model.map.Country;
 
 public class Benevolent extends ConcreteStrategy implements Strategy {
 
 	@Override
 	public void reinforce() {
-		// TODO Auto-generated method stub
-		map.getPhase().setPhase("Reinforcement phase", player);
-		while(player.getArmies() != 0)
+		do
 		{
+			map.getPhase().setPhase("Reinforcement phase", player);
 			Country weakestCountry = player.getWeakestCountry();
-			int armies = player.getArmies();
 			player.reinforcementMove(weakestCountry, 1);
-		}
-		
+		}while(player.getArmies() > 0);
 	}
 
 	@Override
 	public void attack() {
-		// TODO Auto-generated method stub
 		map.getPhase().setPhase("Attack phase", player);
-		System.out.println("Skipped");
-		
 	}
 
 	@Override
 	public void fortify() {
-		// TODO Auto-generated method stub
-		map.getPhase().setPhase("fortification phase", player);
+		map.getPhase().setPhase("Fortification phase", player);
 		
+		Country weakestCountry = player.getWeakestCountry();
+		Country origin = null;
 		
-	}
+		for (Country c : player.ownedCountries) {
+			//Check if a connected country can reinforce the weakest
+			if (c.isConnectedTo(weakestCountry) && c.getArmyNumber() > 1) {
+				origin = c;
+				break;
+			}
+		}
 
+		if (origin == null)
+			return;
+
+		int armies = origin.getArmyNumber() / 2;
+		player.fortificationMove(origin, weakestCountry, armies);
+	}
 }
